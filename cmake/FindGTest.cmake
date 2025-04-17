0cebbabb87e500f1caa9203d2c5fa9a7dc78a56d
Daniel Dube <danieldube@users.noreply.github.com>
2025-04-18 01:01:40 +0200

    Fix google test fetch.

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
-# cmake-lint: disable=C0301
 FetchContent_Declare(
   googletest
-  URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG 6910c9d # v1.16.0
 )
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(GTEST_FORCE_SHARED_CRT
