2af79648579ad773f39d83f47f243a3f454a5020
Daniel Dube <danieldube@users.noreply.github.com>
2025-05-13 23:47:37 +0200

    Implement a primitive builder.

---
+++ src/action_graph/builder.cpp
@@ -0,0 +1,23 @@
+#include <action_graph/action.h>
+#include <action_graph/builder.h>
+
+namespace action_graph {
+namespace builder {
+
+std::vector<ActionObject>
+BuildActionGraph(const std::string &yaml_string,
+                 const ActionCreators &action_creators) {
+  std::vector<ActionObject> created_actions;
+  YAML::Node config = YAML::Load(yaml_string);
+  auto action = config["trigger"]["action"];
+  auto action_type = action["type"].as<std::string>();
+  auto creator = action_creators.find(action_type);
+  if (creator == action_creators.end()) {
+    throw std::runtime_error("Action type not found: " + action_type);
+  }
+  created_actions.push_back(creator->second(action));
+  return created_actions;
+}
+
+} // namespace builder
+} // namespace action_graph
