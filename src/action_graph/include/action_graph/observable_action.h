946b5c40842955a4c236f7b6f044aad95f7bdb23
Daniel Dube <danieldube@users.noreply.github.com>
2025-07-20 18:51:43 +0200

    Correctly implement a dynamic decorator pattern.

--- src/action_graph/include/action_graph/observable_action.h
+++ src/action_graph/include/action_graph/observable_action.h
@@ -1,23 +1,22 @@
 #ifndef ACTION_GRAPH_SRC_ACTION_GRAPH_INCLUDE_ACTION_GRAPH_OBSERVABLE_ACTION_H_
 #define ACTION_GRAPH_SRC_ACTION_GRAPH_INCLUDE_ACTION_GRAPH_OBSERVABLE_ACTION_H_
 
-#include <action_graph/action.h>
+#include <action_graph/decorated_action.h>
 #include <action_graph/execution_observer.h>
 #include <memory>
 
 namespace action_graph {
-class ObservableAction : public Action {
+class ObservableAction : public DecoratedAction {
 public:
   ObservableAction(std::unique_ptr<Action> action,
                    std::unique_ptr<ExecutionObserver> observer)
-      : Action(action->name), observer_(std::move(observer)),
-        action_(std::move(action)) {}
+      : DecoratedAction(std::move(action)), observer_(std::move(observer)) {}
 
   void Execute() override {
     observer_->OnStarted();
 
     try {
-      action_->Execute();
+      GetAction().Execute();
     } catch (std::exception &exception) {
       observer_->OnFailed(exception);
       throw;
@@ -27,7 +26,6 @@ public:
 
 private:
   std::unique_ptr<ExecutionObserver> observer_;
-  std::unique_ptr<Action> action_;
 };
 } // namespace action_graph
 
