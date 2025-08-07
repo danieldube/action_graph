dc6258c1ee9939a956f422a58f3b3507a601a8a8
Daniel Dube <danieldube@users.noreply.github.com>
2025-08-08 01:12:38 +0200

    Add pre-commit hook to add headers to all files.

--- src/action_graph/action.cpp
+++ src/action_graph/action.cpp
@@ -1,1 +1,6 @@
+// Copyright (c) 2025 Daniel Dube
+//
+// This file is part of the action_graph library and is licensed under the MIT
+// License. See the LICENSE file in the root directory for full license text.
+
 void test() {}
--- src/action_graph/include/action_graph/observable_action.h
+++ src/action_graph/include/action_graph/observable_action.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Daniel Dube
+//
+// This file is part of the action_graph library and is licensed under the MIT
+// License. See the LICENSE file in the root directory for full license text.
+
 #ifndef ACTION_GRAPH_SRC_ACTION_GRAPH_INCLUDE_ACTION_GRAPH_OBSERVABLE_ACTION_H_
 #define ACTION_GRAPH_SRC_ACTION_GRAPH_INCLUDE_ACTION_GRAPH_OBSERVABLE_ACTION_H_
 
