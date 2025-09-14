18f6f757087ed4766898da81f96b745a38d43151
Daniel Dube <danieldube@users.noreply.github.com>
2025-09-14 16:28:49 +0200

    Revert files to the original state.

--- tests/stress_tests/action_sequence_performance_test.cpp
+++ tests/stress_tests/action_sequence_performance_test.cpp
@@ -1,4 +1,3 @@
-// ...existing code...
 // Copyright (c) 2025 Daniel Dube
 //
 // This file is part of the action_graph library and is licensed under the MIT
--- tests/stress_tests/parallel_actions_performance_test.cpp
+++ tests/stress_tests/parallel_actions_performance_test.cpp
@@ -7,45 +7,56 @@
 #include <action_graph/single_action.h>
 #include <algorithm>
 #include <gtest/gtest.h>
-#include <thread>
 
 using action_graph::Action;
 using action_graph::ParallelActions;
 using action_graph::SingleAction;
 
 TEST(ParallelActionsPerformance, many_actions) {
-  constexpr std::size_t kActionsCount = 10000;
-
+  constexpr std::size_t kActionsCount = 1000;
   std::vector<std::unique_ptr<Action>> actions;
   actions.reserve(kActionsCount);
-
-  std::atomic<size_t> counter = 0;
+  std::atomic<std::size_t> counter = 0;
   std::generate_n(std::back_inserter(actions), kActionsCount, [&counter]() {
     return std::make_unique<SingleAction>("action",
                                           [&counter]() { ++counter; });
   });
-  ParallelActions parallel("parallel", std::move(actions));
-
-  parallel.Execute();
-
+  action_graph::ParallelActions sequence("sequence", std::move(actions));
+  sequence.Execute();
   EXPECT_EQ(counter, kActionsCount);
 }
 
-TEST(ParallelActionsPerformance, many_threads) {
-  constexpr std::size_t kThreadCount = 100;
-  std::size_t counter = 0;
+TEST(ParallelActionsPerformance, many_iterations) {
+  constexpr std::size_t kIterationCount = 1000;
+  std::atomic<std::size_t> counter = 0;
+  action_graph::ParallelActions sequence(
+      "sequence",
+      std::make_unique<SingleAction>("action", [&counter]() { ++counter; }));
+
+  for (size_t iteration = 0; iteration < kIterationCount; ++iteration) {
+    sequence.Execute();
+  }
+
+  EXPECT_EQ(counter, kIterationCount);
+}
+
+TEST(ParallelActionsPerformance, many_actions_many_iterations) {
+  constexpr std::size_t kActionsCount = 100;
+  constexpr std::size_t kIterationCount = 100;
 
   std::vector<std::unique_ptr<Action>> actions;
-  actions.reserve(kThreadCount);
-  for (size_t i = 0; i < kThreadCount; ++i) {
-    actions.push_back(std::make_unique<SingleAction>("action", [&counter]() {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-      ++counter;
-    }));
+  actions.reserve(kActionsCount);
+
+  std::atomic<std::size_t> counter = 0;
+
+  std::generate_n(std::back_inserter(actions), kActionsCount, [&counter]() {
+    return std::make_unique<SingleAction>("action",
+                                          [&counter]() { ++counter; });
+  });
+  action_graph::ParallelActions sequence("sequence", std::move(actions));
+
+  for (size_t iteration = 0; iteration < kIterationCount; ++iteration) {
+    sequence.Execute();
   }
-  ParallelActions parallel("parallel", std::move(actions));
-
-  parallel.Execute();
-
-  EXPECT_EQ(counter, kThreadCount);
+  EXPECT_EQ(counter, kActionsCount * kIterationCount);
 }
